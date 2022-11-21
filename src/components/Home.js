47e2544a9a89f4456657e6253caa02872a2fc5de
Gopi Krishna <srungarapugopikrishna@gmail.com>
2022-11-21 18:43:43 +0530

    Adding posts to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,20 @@
 import * as React from "react";
 import CrowdFundApp from "../CrowdFundingApp.json";
 import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 export default function Home() {
   const sampleData = [
@@ -28,6 +42,14 @@ export default function Home() {
   ];
   const [data, updateData] = useState(sampleData);
   const [dataFetched, updateFetched] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const fund = formData.get("funding");
+    alert(`Fund=${fund}`);
+  };
+
   async function getAllPosts() {
     console.log("-====-In getAllPosts-====-");
     const ethers = require("ethers");
@@ -60,10 +82,33 @@ export default function Home() {
   if (!dataFetched) getAllPosts();
 
   return (
-    <div>
-      {data.map((value, index) => {
-        return data[index].postTitle + "<>" + data[index].postDescription;
-      })}
-    </div>
+    <Box sx={{ width: "100%" }}>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 2, sm: 3, md: 4 }}>
+        {data.map((value, index) => {
+          // return data[index].postTitle + "<>" + data[index].postDescription;
+          return (
+            <Grid item xs={6}>
+              <Item>Title: {data[index].postTitle} </Item>
+              <Item>Description: {data[index].postDescription}</Item>
+              <Item>Amount Collected: 0</Item>
+              <Item>
+                <form onSubmit={handleSubmit}>
+                  <TextField
+                    label="ETH"
+                    id={data[index].postId}
+                    name="funding"
+                    defaultValue="0.01"
+                    size="small"
+                  />
+                  <Button variant="contained" type="submit">
+                    Fund
+                  </Button>
+                </form>
+              </Item>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
   );
 }
